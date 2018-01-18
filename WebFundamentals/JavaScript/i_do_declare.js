6ed9539608d42b111fa56178b46331f6617897ae
Jeff Kloythanomsup <jkloythanomsup@corpinfo.com>
2018-01-18 11:50:20 -0800

    I do declare

---
+++ WebFundamentals/JavaScript/i_do_declare.js
@@ -0,0 +1,13 @@
+var num1 = 42;
+var num2 = 3;
+var num3 = 777;
+var num4 = 500;
+
+var string1 = "Hello, World.";
+var string2 = "TESTING! 1 2 3";
+var string3 = "Goodbye, World.";
+
+var bool1 = true;
+var bool2 = false;
+
+var undefinedVar;
