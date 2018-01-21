4e38ebc3b3cc57f1282e0834174b50b108cb83e4
Jeff Kloythanomsup <jeff.kloythanomsup@polybuilder.io>
2018-01-21 13:06:50 -0800

    Random Chance fix for 0

--- WebFundamentals/JavaScript/random_chance.js
+++ WebFundamentals/JavaScript/random_chance.js
@@ -1,10 +1,12 @@
 // This assignment could be entirely incorrect as I didn't really understand the prompt.
 
 function randomChance (quarters) {
-    var q = Math.random(quarters) * 100;
-    q = Math.floor(q);
-    var total = quarters + q;
-    console.log("You started with " + quarters + " and won " + q + ", with a total of " + total + ".");
+    if (quarters > 1) {
+        var q = Math.random(quarters) * 100;
+        q = Math.floor(q);
+        var total = quarters + q;
+        console.log("You started with " + quarters + " and won " + q + ", with a total of " + total + ".");
+    } else {
+        console.log("You have no money.");
+    }
 }
-
-randomChance(1);
