005c1ff3cd8b1fa01050d465c8509e5ca78b1c51
Jeff Kloythanomsup <jeff.kloythanomsup@polybuilder.io>
2018-02-08 19:39:15 -0800

    Making dicts assignment

---
+++ Python/making_dicts.py
@@ -0,0 +1,9 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+def make_dict(list1, list2):
+    new_dict = {}
+    new_dict = zip(list1, list2)
+    print new_dict
+
+make_dict(name, favorite_animal)
