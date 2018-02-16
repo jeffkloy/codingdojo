7ee3d9b12a680d3ec6c53377c28110ae7315b89c
Jeff Kloythanomsup <jkloythanomsup@corpinfo.com>
2018-02-15 19:04:03 -0800

    Sessions part 1 - pre-virtualenv Flask install

--- Python/Flask/form_test/server.py
+++ Python/Flask/form_test/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'cd4f5e2a8c458a44c0683f8b22304c8675fb9ee6f2e5675c38417ad2f4252b45'
 
 @app.route('/')
 def index():
@@ -17,8 +18,8 @@ def success():
 @app.route('/users', methods=['POST'])
 def create_user():
     print "Got Post Info"
-    name = request.form['name']
-    email = request.form['email']
+    session['name'] = request.form['name']
+    session['email'] = request.form['email']
     return redirect('/success')
 
 @app.route('/users/<username>')
@@ -28,6 +29,6 @@ def show_user_profile(username):
 
 @app.route('/show')
 def show_user():
-    return render_template('user.html', name='Jay', email='kpatel@codingdojo.com')
+    return render_template('user.html', name=session['name'], email=session['email'])
 
 app.run(debug=True)
