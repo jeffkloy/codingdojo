d3edab9565a1f0b442f893ed25af5017e5dd8049
Jeff Kloythanomsup <jeff.kloythanomsup@polybuilder.io>
2018-04-16 02:43:07 -0700

    Old commits and new JS Basics I thing from MEAN stack pages

---
+++ MEAN/JavaScript/javascript-basics-i.js
@@ -0,0 +1,25 @@
+// BASIC 1
+x = [];
+console.log(x);
+x.push('coding', 'dojo', 'rocks');
+x.pop();
+console.log(x);
+
+// BASIC 2
+const y = [];
+console.log(y);
+y.push(88);
+console.log(y);
+
+// BASIC 3
+let z = [9, 10, 6, 5, -1, 20, 13, 2];
+console.log(z);
+z.pop();
+console.log(z);
+
+// BASIC 4
+function yell (string) {
+    console.log(string.toUpperCase());
+    return string.toUpperCase();
+}
+yell('this is easy');
