d3ced07ffe0fd2e47103378a4e5c7b9b109413da
Jeff Kloythanomsup <jeff.kloythanomsup@polybuilder.io>
2018-04-21 20:56:10 -0700

    Python OOP call centre assignment updated - INCOMPLETE

--- Python/OOP/callcentre.py
+++ Python/OOP/callcentre.py
@@ -1,30 +1,31 @@
+from datetime import datetime
+
 class Call(object):
-    def __init__(self, unique_id, name, number, time, reason):
+    def __init__(self, unique_id, name, number, reason):
         self.id = unique_id
         self.name = name
         self.number = number
-        self.time = time
+        self.time = datetime.now()
         self.reason = reason
     def display(self):
         print "Unique ID:", self.id, "Name:", self.name, "Phone number:", self.number, "Time:", self.time, "Reason:", self.reason
 
-class CallCentre(Call):
-    def __init__(self, calls, queue):
+class CallCentre(object):
+    def __init__(self):
         super(CallCentre, self).__init__()
         self.calls = []
         self.queue = len(self.calls)
-    def add(self):
-        self.calls.append()
-    def remove(self):
-        self.calls.pop()
+    def add(self, call):
+        self.calls.append(self.calls)
+    def remove(self, call):
+        self.calls.remove(self.calls)
     def info(self):
-        print "Name:", self.name, "Phone number:", self.number, "Queue length:", self.calls
+        print "Name:", name, "Phone number:", number, "Queue length:", calls
 
-call1 = Call(1, "Jeff", 14157452146, 201804211927, "Callback")
+call1 = Call(1, "Jeff K.", "+1 415-555-1212", "Callback")
 call1.display()
 
-callcentre1 = CallCentre(0, 0)
-callcentre1.add()
-print callcentre1
+callcentre1 = CallCentre()
+callcentre1.add(1)
 callcentre1.remove()
 print callcentre1.info()
